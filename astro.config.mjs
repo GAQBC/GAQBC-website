d6960b270ac5dafad63cb1eb2edea22fee9c3a14
Pellucid1137 <pellucid1137@gmail.com>
2025-07-13 14:07:15 -0400

    wipe everything!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
+
 export default defineConfig({
   site: "https://mitosisphere.org",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
